33e37edc29c6c18e511bdd65eda8b03b995de432
demba003 <kd.debinski@gmail.com>
2018-08-29 18:11:52 +0200

    add AssetLoader & legal notices entry

---
+++ app/src/main/java/com/demba/localareanavigator/utils/AssetLoader.java
@@ -0,0 +1,24 @@
+package com.demba.localareanavigator.utils;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+public class AssetLoader {
+
+    private static final String DELIMITER_NEW_LINE = "\n";
+
+    public static String loadGeoJson(@NonNull Context context, String filename) throws IOException{
+        InputStream inputStream = context.getAssets().open(filename);
+        return new BufferedReader(
+                new InputStreamReader(inputStream))
+                    .lines()
+                    .parallel()
+                    .collect(Collectors.joining(DELIMITER_NEW_LINE));
+    }
+}
