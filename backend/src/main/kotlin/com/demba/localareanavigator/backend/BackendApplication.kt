702179ace70cf819c6bd6dc8f7033b679feb9c23
demba003 <kd.debinski@gmail.com>
2018-11-02 17:33:07 +0100

    backend: heroku support

--- backend/src/main/kotlin/com/demba/localareanavigator/backend/BackendApplication.kt
+++ backend/src/main/kotlin/com/demba/localareanavigator/backend/BackendApplication.kt
@@ -2,10 +2,25 @@ package com.demba.localareanavigator.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory
+import org.springframework.boot.web.server.WebServerFactoryCustomizer
+import org.springframework.stereotype.Component
 
 @SpringBootApplication
 class BackendApplication
 
 fun main(args: Array<String>) {
     runApplication<BackendApplication>(*args)
+}
+
+@Component
+class CustomContainer : WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> {
+
+    override fun customize(factory: ConfigurableServletWebServerFactory) {
+        factory.setContextPath("")
+        if(System.getenv("PORT")!=null) {
+            factory.setPort(Integer.valueOf(System.getenv("PORT")))
+
+        }
+    }
 }
--- backend/src/main/kotlin/com/demba/localareanavigator/backend/PlacesController.kt
+++ backend/src/main/kotlin/com/demba/localareanavigator/backend/PlacesController.kt
@@ -1,10 +1,12 @@
 package com.demba.localareanavigator.backend
 
 import com.demba.localareanavigator.backend.models.Place
-import org.springframework.util.ResourceUtils
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
+import java.io.InputStreamReader
+import java.io.BufferedReader
+import org.springframework.core.io.ClassPathResource
 
 @RestController
 class PlacesController {
@@ -18,8 +20,9 @@ class PlacesController {
 
     @GetMapping("/places/{id}")
     fun place(@PathVariable id: String): Place {
-        val file = ResourceUtils.getFile("classpath:$id.min.geojson")
+        val resource = ClassPathResource("$id.min.geojson")
+        val reader = BufferedReader(InputStreamReader(resource.inputStream))
         val url = "https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/19.942,50.0719,16/600x400?access_token=pk.eyJ1IjoiZGVtYmEiLCJhIjoiY2pibWo3cW43M2I5eDM0cjY0eG4zY2JxZyJ9.SFBv4D82Ih54yJHF5U__BQ"
-        return Place(id, file.readText(), url)
+        return Place(id, reader.use { it.readText() }, url)
     }
 }
