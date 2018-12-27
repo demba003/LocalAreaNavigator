7ad138149c24e89d519b7060f326034877447fdf
demba003 <kd.debinski@gmail.com>
2018-12-27 19:04:12 +0100

    backend: store places as files and add upload support

--- backend/src/main/kotlin/com/demba/localareanavigator/backend/PlacesController.kt
+++ backend/src/main/kotlin/com/demba/localareanavigator/backend/PlacesController.kt
@@ -1,29 +1,31 @@
 package com.demba.localareanavigator.backend
 
 import com.demba.localareanavigator.backend.models.Place
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RestController
-import java.io.InputStreamReader
-import java.io.BufferedReader
-import org.springframework.core.io.ClassPathResource
+import org.springframework.web.bind.annotation.*
+import java.io.File
 
 @RestController
 class PlacesController {
 
-    @GetMapping("/places")
-    fun places(): Array<Place> {
+    @GetMapping("/place")
+    fun getPlaces(): List<Place> {
         val url = "https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/19.942,50.0719,16/600x400?access_token=pk.eyJ1IjoiZGVtYmEiLCJhIjoiY2pibWo3cW43M2I5eDM0cjY0eG4zY2JxZyJ9.SFBv4D82Ih54yJHF5U__BQ"
-        val place = Place("kampus", null, url)
-        val place2 = Place("kampus witch", null, url)
-        return arrayOf(place, place2, place, place2)
+
+        return File(".")
+                .listFiles()
+                .asSequence()
+                .filter { it.name.contains("json") }
+                .map { Place(it.name.substring(0, it.name.indexOf(".")), null, url) }
+                .toList()
     }
 
-    @GetMapping("/places/{id}")
-    fun place(@PathVariable id: String): Place {
-        val resource = ClassPathResource("$id.min.geojson")
-        val reader = BufferedReader(InputStreamReader(resource.inputStream))
-        val url = "https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/19.942,50.0719,16/600x400?access_token=pk.eyJ1IjoiZGVtYmEiLCJhIjoiY2pibWo3cW43M2I5eDM0cjY0eG4zY2JxZyJ9.SFBv4D82Ih54yJHF5U__BQ"
-        return Place(id, reader.use { it.readText() }, url)
+    @GetMapping(value = ["/place/{name}"], produces = ["application/json"])
+    fun getPlace(@PathVariable name: String): String {
+        return File("$name.json").readText()
+    }
+
+    @PutMapping("/place")
+    fun postPlace(@RequestBody place: Place) {
+        File("${place.name}.json").writeText(place.toJson())
     }
 }
--- backend/src/main/kotlin/com/demba/localareanavigator/backend/models/Place.kt
+++ backend/src/main/kotlin/com/demba/localareanavigator/backend/models/Place.kt
@@ -1,3 +1,8 @@
 package com.demba.localareanavigator.backend.models
 
-data class Place(val name: String, val data: String?, val imageURL: String)
+data class Place(val name: String, val data: String?, val imageURL: String) {
+
+    fun toJson(): String {
+        return "{\"name\":\"$name\",\"data\":\"${data?.replace("\"", "\\\"")}\",\"imageURL\":\"$imageURL\"}"
+    }
+}
